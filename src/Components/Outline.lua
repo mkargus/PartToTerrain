8ae33e1b5678a6f81618f55ff23c12451f7c4df8
michael <14226603+mkargus@users.noreply.github.com>
2021-06-23 10:16:01 -0400

    Outline takes "IgnoreLockedParts" setting

--- src/Components/Outline.lua
+++ src/Components/Outline.lua
@@ -7,6 +7,8 @@ local Constants = require(Util.Constants)
 local TerrainEnum = require(Util.TerrainEnum)
 local TerrainUtil = require(Util.TerrainUtil)
 
+local PluginSettings = require(Plugin.Components.PluginSettings)
+
 local Outline = Roact.PureComponent:extend('Outline')
 
 function Outline:init()
@@ -51,49 +53,61 @@ function Outline:render()
 
   local isConvertibleToTerrain = TerrainUtil.isConvertibleToTerrain(Part)
 
-  self.props.PluginMouse.Icon = isConvertibleToTerrain and '' or 'rbxasset://SystemCursors/Forbidden'
+  return PluginSettings.with(function(settings)
 
-  local color = (function()
-    if isConvertibleToTerrain then
-      return #Part:GetChildren() == 0  and Constants.OUTLINE_COLOR_ALLOW or Constants.OUTLINE_COLOR_WARNING
-    else
-      return Constants.OUTLINE_COLOR_ERROR
+    local function isLockedPartAllowed()
+      local setting = settings:Get('IgnoreLockedParts')
+      if setting then
+        return setting == Part.Locked
+      else
+        return false
+      end
     end
-  end)()
 
-  return Roact.createElement(Roact.Portal, {
-    target = game.CoreGui
-  }, {
+    self.props.PluginMouse.Icon = (isConvertibleToTerrain and not isLockedPartAllowed()) and '' or 'rbxasset://SystemCursors/Forbidden'
 
-    PTT_BoxHandleAdornment = (shape == TerrainEnum.Shape.Block or shape == TerrainEnum.Shape.Wedge) and Roact.createElement('BoxHandleAdornment', {
-      Adornee = Part,
-      AlwaysOnTop = true,
-      Color3 = color,
-      Size = Part.Size,
-      Transparency = 0.3,
-      ZIndex = 1
-    }),
+    local color = (function()
+      if isConvertibleToTerrain and not isLockedPartAllowed() then
+        return #Part:GetChildren() == 0  and Constants.OUTLINE_COLOR_ALLOW or Constants.OUTLINE_COLOR_WARNING
+      else
+        return Constants.OUTLINE_COLOR_ERROR
+      end
+    end)()
 
-    PTT_SphereHandleAdornment = shape == TerrainEnum.Shape.Ball and Roact.createElement('SphereHandleAdornment', {
-      Adornee = Part,
-      AlwaysOnTop = true,
-      Color3 = color,
-      Radius = math.min(Part.Size.X, Part.Size.Y, Part.Size.Z) / 2,
-      Transparency = 0.3,
-      ZIndex = 1
-    }),
+    return Roact.createElement(Roact.Portal, {
+      target = game.CoreGui
+    }, {
 
-    CylinderHandleAdornment = shape == TerrainEnum.Shape.CylinderRotate and Roact.createElement('CylinderHandleAdornment', {
-      Adornee = Part,
-      AlwaysOnTop = true,
-      CFrame = CFrame.new() * CFrame.Angles(0, math.rad(90), 0),
-      Color3 = color,
-      Height = Part.Size.X,
-      Radius = math.min(Part.Size.Y, Part.Size.Z) / 2,
-      Transparency = 0.3,
-      ZIndex = 1
+      PTT_BoxHandleAdornment = (shape == TerrainEnum.Shape.Block or shape == TerrainEnum.Shape.Wedge) and Roact.createElement('BoxHandleAdornment', {
+        Adornee = Part,
+        AlwaysOnTop = true,
+        Color3 = color,
+        Size = Part.Size,
+        Transparency = 0.3,
+        ZIndex = 1
+      }),
+
+      PTT_SphereHandleAdornment = shape == TerrainEnum.Shape.Ball and Roact.createElement('SphereHandleAdornment', {
+        Adornee = Part,
+        AlwaysOnTop = true,
+        Color3 = color,
+        Radius = math.min(Part.Size.X, Part.Size.Y, Part.Size.Z) / 2,
+        Transparency = 0.3,
+        ZIndex = 1
+      }),
+
+      CylinderHandleAdornment = shape == TerrainEnum.Shape.CylinderRotate and Roact.createElement('CylinderHandleAdornment', {
+        Adornee = Part,
+        AlwaysOnTop = true,
+        CFrame = CFrame.new() * CFrame.Angles(0, math.rad(90), 0),
+        Color3 = color,
+        Height = Part.Size.X,
+        Radius = math.min(Part.Size.Y, Part.Size.Z) / 2,
+        Transparency = 0.3,
+        ZIndex = 1
+      })
     })
-  })
+  end)
 end
 
 function Outline:willUnmount()
