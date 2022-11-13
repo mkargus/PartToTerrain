081e5768951430c1289530e6ca7180f16c3524e7
michael <14226603+mkargus@users.noreply.github.com>
2022-11-13 14:38:23 -0500

    Misc changes to SettingsItem
    - Description FontSize is now 14
    - Fixed issue an issue where if the title would collide with description
    - StudioTheme uses the context directly

--- src/Components/SettingsPanel/Item.lua
+++ src/Components/SettingsPanel/Item.lua
@@ -6,8 +6,9 @@ local Util = Plugin.Util
 local Localization = require(Util.Localization)
 local Settings = require(Util.Settings)
 
+local StudioTheme = require(Plugin.Context.StudioTheme)
+
 local Components = Plugin.Components
-local StudioTheme = require(Components.StudioTheme)
 local TextLabel = require(Components.TextLabel)
 local ToggleButton = require(Components.ToggleButton)
 
@@ -35,36 +36,50 @@ function SettingsItem:render()
       LayoutOrder = props.LayoutOrder,
       Size = UDim2.fromScale(0.95, 0),
     }, {
-      Title = Roact.createElement(TextLabel, {
-        AutomaticSize = Enum.AutomaticSize.Y,
-        BackgroundTransparency = 1,
-        Font = Enum.Font.GothamBold,
-        Size = UDim2.new(0.8, 0, 0, 24),
-        Text = Localization('Settings.'..props.Title),
-        TextColor3 = theme:GetColor(Enum.StudioStyleGuideColor.MainText),
-        TextSize = 16,
-        TextWrapped = true,
-        TextXAlignment = Enum.TextXAlignment.Left,
+      UIListLayout = Roact.createElement('UIListLayout', {
+        FillDirection = Enum.FillDirection.Vertical,
+        HorizontalAlignment = Enum.HorizontalAlignment.Center,
+        Padding = UDim.new(0, 3),
+        SortOrder = Enum.SortOrder.LayoutOrder
       }),
 
-      Toggle = Roact.createElement(ToggleButton, {
-        AnchorPoint = Vector2.new(1, 0),
-        Position = UDim2.fromScale(1, 0),
-        isEnabled = currentSettingValue,
-        onClick = function()
-          Settings:Set(props.Title, not currentSettingValue)
-        end
+      Top = Roact.createElement('Frame', {
+        AutomaticSize = Enum.AutomaticSize.Y,
+        BackgroundTransparency = 1,
+        Size = UDim2.fromScale(1, 0)
+      }, {
+        Title = Roact.createElement(TextLabel, {
+          AutomaticSize = Enum.AutomaticSize.Y,
+          BackgroundTransparency = 1,
+          Font = Enum.Font.GothamBold,
+          Size = UDim2.new(1, -43, 0, 24),
+          Text = Localization('Settings.'..props.Title),
+          TextColor3 = theme:GetColor(Enum.StudioStyleGuideColor.MainText),
+          TextSize = 16,
+          TextWrapped = true,
+          TextXAlignment = Enum.TextXAlignment.Left,
+        }),
+
+        Toggle = Roact.createElement(ToggleButton, {
+          AnchorPoint = Vector2.new(1, 0),
+          Position = UDim2.fromScale(1, 0),
+          isEnabled = currentSettingValue,
+          onClick = function()
+            Settings:Set(props.Title, not currentSettingValue)
+          end
+        })
       }),
 
       Description = Roact.createElement(TextLabel, {
         AutomaticSize = Enum.AutomaticSize.Y,
         BackgroundTransparency = 1,
         LineHeight = 1.2,
+        LayoutOrder = 1,
         Position = UDim2.fromOffset(0, 30),
         Size = UDim2.fromScale(1, 0),
         Text = Localization('Settings.'..props.Title..'Desc'),
         TextColor3 = theme:GetColor(Enum.StudioStyleGuideColor.SubText),
-        TextSize = 12,
+        TextSize = 14,
         TextWrapped = true,
         TextXAlignment = Enum.TextXAlignment.Left,
         RichText = true
