480f0943522627466aa4fffd5729cb6e3398780d
michael <14226603+mkargus@users.noreply.github.com>
2023-11-20 14:44:44 -0500

    Recoded Outline

--- src/Components/Outline.lua
+++ src/Components/Outline.lua
@@ -1,6 +1,7 @@
 local Plugin = script.Parent.Parent
 
 local Roact = require(Plugin.Packages.Roact)
+local Hooks = require(Plugin.Packages.RoactHooks)
 
 local Util = Plugin.Util
 local Constants = require(Util.Constants)
@@ -8,39 +9,7 @@ local Settings = require(Util.Settings)
 local TerrainEnum = require(Util.TerrainEnum)
 local TerrainUtil = require(Util.TerrainUtil)
 
-local Outline = Roact.PureComponent:extend('Outline')
-
-function Outline:init()
-  self.state = {
-    part = nil
-  }
-
-  self.PluginMouse = self.props.PluginMouse :: PluginMouse
-
-  self.MoveConnection = self.PluginMouse.Move:Connect(function()
-    local camera = workspace.CurrentCamera.CFrame
-    local ray = self.props.PluginMouse.UnitRay
-    local RaycastResults = workspace:Raycast(camera.Position, ray.Direction * 15000, self.props.raycastParams)
-
-    if RaycastResults and not RaycastResults.Instance:IsA('Terrain') then
-
-      -- Only change the state if the part in the RaycastResults is different from the one in the state.
-      if RaycastResults.Instance ~= self.state.part then
-        self:setState({ part = RaycastResults.Instance })
-      end
-
-    else
-
-      if self.state.part then
-        self.props.PluginMouse.Icon = ''
-        self:setState({ part = Roact.None })
-      end
-
-    end
-  end)
-end
-
-function IsLockedPartAllowed(Part)
+local function IsLockedPartAllowed(Part)
   local setting = Settings:Get('IgnoreLockedParts')
   if setting then
     return setting == Part.Locked
@@ -49,69 +18,93 @@ function IsLockedPartAllowed(Part)
   end
 end
 
-function Outline:render()
-  local state = self.state
-  local Part = state.part
+local function GetColor(Part, isConvertibleToTerrain, isLockedPartAllowed)
+  if isConvertibleToTerrain and not isLockedPartAllowed then
+    return #Part:GetChildren() == 0 and Constants.OUTLINE_COLOR_ALLOW or Constants.OUTLINE_COLOR_WARNING
+  else
+    return Constants.OUTLINE_COLOR_ERROR
+  end
+end
+
+local function CreateOutline(Part, color)
+  local shape, _, size = TerrainUtil:GetPartInfo(Part)
+
+  local DEFAULT_OUTLINE_PROPS = {
+    Adornee = Part,
+    AlwaysOnTop = true,
+    Color3 = color,
+    Transparency = 0.3,
+    ZIndex = 1
+  }
+
+  local props = table.clone(DEFAULT_OUTLINE_PROPS)
+
+  if shape == TerrainEnum.Shape.Block or shape == TerrainEnum.Shape.Wedge then
+    props.Size = size
+    return 'BoxHandleAdornment', props
+  elseif shape == TerrainEnum.Shape.Ball then
+    props.Radius = math.min(size.X, size.Y, size.Z) / 2
+    return 'SphereHandleAdornment', props
+  elseif shape == TerrainEnum.Shape.CylinderRotate then
+    props.CFrame = CFrame.new() * CFrame.Angles(0, math.rad(90), 0)
+    props.Height = size.X
+    props.Radius = math.min(size.Y, size.Z) / 2
+    return 'CylinderHandleAdornment', props
+  else
+    print('Unknown Shape outline requested. Returning Block shape.')
+    props.Size = size
+    return 'BoxHandleAdornment', props
+  end
+end
+
+local function Outline(props, hooks)
+  local Part, setPart = hooks.useState()
+  local PluginMouse = props.PluginMouse :: PluginMouse
+
+  hooks.useEffect(function()
+    local MoveConnection = PluginMouse.Move:Connect(function()
+      local camera = workspace.CurrentCamera.CFrame
+      local ray = PluginMouse.UnitRay
+      local RaycastResults = workspace:Raycast(camera.Position, ray.Direction * 15000, props.raycastParams)
+
+      if RaycastResults and not RaycastResults.Instance:IsA('Terrain') then
+        -- Only change the state if the part in the RaycastResults is different from the one in the state.
+        if RaycastResults.Instance ~= Part then
+          setPart(RaycastResults.Instance)
+        end
+      else
+        setPart(nil)
+      end
+    end)
+
+    return function()
+      MoveConnection:Disconnect()
+    end
+  end, {})
 
   if not Part then
-    self.PluginMouse.Icon = 'rbxasset://SystemCursors/Arrow'
+    PluginMouse.Icon = 'rbxasset://SystemCursors/Arrow'
     return nil
   end
 
-  local shape, _, size = TerrainUtil:GetPartInfo(Part)
   local isConvertibleToTerrain = TerrainUtil:IsConvertibleToTerrain(Part)
   local isLockedPartAllowed = IsLockedPartAllowed(Part)
 
-  self.PluginMouse.Icon = (isConvertibleToTerrain and not isLockedPartAllowed) and '' or 'rbxasset://SystemCursors/Forbidden'
+  PluginMouse.Icon = (isConvertibleToTerrain and not isLockedPartAllowed) and '' or 'rbxasset://SystemCursors/Forbidden'
 
   -- If the setting is disabled, then no need to do anymore work.
   if not Settings:Get('EnabledSelectionBox') then return nil end
 
-  local color = (function()
-    if isConvertibleToTerrain and not isLockedPartAllowed then
-      return #Part:GetChildren() == 0 and Constants.OUTLINE_COLOR_ALLOW or Constants.OUTLINE_COLOR_WARNING
-    else
-      return Constants.OUTLINE_COLOR_ERROR
-    end
-  end)()
+  local color = GetColor(Part, isConvertibleToTerrain, isLockedPartAllowed)
+  local outlineClass, outlineProps = CreateOutline(Part, color)
 
   return Roact.createElement(Roact.Portal, {
     target = game:GetService('CoreGui')
   }, {
-
-    PTT_BoxHandleAdornment = (shape == TerrainEnum.Shape.Block or shape == TerrainEnum.Shape.Wedge) and Roact.createElement('BoxHandleAdornment', {
-      Adornee = Part,
-      AlwaysOnTop = true,
-      Color3 = color,
-      Size = size,
-      Transparency = 0.3,
-      ZIndex = 1
-    }),
-
-    PTT_SphereHandleAdornment = shape == TerrainEnum.Shape.Ball and Roact.createElement('SphereHandleAdornment', {
-      Adornee = Part,
-      AlwaysOnTop = true,
-      Color3 = color,
-      Radius = math.min(size.X, size.Y, size.Z) / 2,
-      Transparency = 0.3,
-      ZIndex = 1
-    }),
-
-    CylinderHandleAdornment = shape == TerrainEnum.Shape.CylinderRotate and Roact.createElement('CylinderHandleAdornment', {
-      Adornee = Part,
-      AlwaysOnTop = true,
-      CFrame = CFrame.new() * CFrame.Angles(0, math.rad(90), 0),
-      Color3 = color,
-      Height = size.X,
-      Radius = math.min(size.Y, size.Z) / 2,
-      Transparency = 0.3,
-      ZIndex = 1
-     })
+    PTT_Outline = Roact.createElement(outlineClass, outlineProps)
   })
 end
 
-function Outline:willUnmount()
-  self.MoveConnection:Disconnect()
-end
+Outline = Hooks.new(Roact)(Outline)
 
 return Outline
